7f6e1ccbb8d687bd25d5b84af26fb80dabee956b
Nilshut <nilshut@web.de>
2020-02-06 21:26:11 +0100

    add subject filter

--- d3/crossfilter.js
+++ d3/crossfilter.js
@@ -5,11 +5,13 @@ export function init(filterData, nodeData, linkData) {
   const durationDim = cf.dimension(d => d.duration);
   const institutionDim = cf.dimension(d => d.institution_id);
   const projectDim = cf.dimension(d => d.project_id_number);
+  const subjectDim = cf.dimension(d => d.subject);
 
   const getProjectIds = () => cf.allFiltered().map(d => projectDim.accessor(d));
   const getUniqueIds = (projectIds) => [...new Set(projectIds)];
   const getFilteredData = () => {
     const uniqueProjectIds = getUniqueIds(getProjectIds());
+    console.log(uniqueProjectIds);
     const nodes = nodeData.filter(d => uniqueProjectIds.includes(d.project_id_number));
     const personIds = nodes.map(node => node.person_id);
     return {
@@ -26,12 +28,31 @@ export function init(filterData, nodeData, linkData) {
     return getFilteredData();
   }
 
+  const filterExact = (dimension) => (val) => {
+    dimension.filterAll();
+    if (val) {
+      dimension.filterExact(val);
+    }
+    return getFilteredData();
+  }
+
+  const labels = (dim, labelAccessor) => {
+    const ids = dim.group().all().map(({ key }) => key);
+    return ids.map(dimId => ({
+      id: dimId,
+      label: cf.all().find(d => institutionDim.accessor(d) === dimId)[labelAccessor]
+    }));
+  }
+
   return {
     filterYear: filterRange(yearDim),
     filterDuration: filterRange(durationDim),
-    filterInstitution: filterRange(institutionDim),
+    filterInstitution: filterExact(institutionDim),
+    filterSubject: filterExact(subjectDim),
     yearGroup: yearDim.group(),
     durationGroup: durationDim.group(),
-    institutionGroup: institutionDim.group()
+    institutionGroup: institutionDim.group(),
+    subjectGroup: subjectDim.group(),
+    institutionLabels: labels(institutionDim, 'institution_name')
   }
 }
